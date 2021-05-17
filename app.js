511052990994e08d1a7e641797a6e1c265f30ac1
Franklin Noriega <zerocreative.f@gmail.com>
2021-05-17 16:02:25 -0400

    touches

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const config = require('config');
 const bot = new Telegraf(config.telegram.key);
 
 bot.start(async (ctx) => {
-  ctx.reply('¡Bienvenid@ ');
+  ctx.reply('¡Bienvenid@ a SeVEx Bot. Este bot fue desarrollado por @franklinzerocr y solo se encarga de darle forward a los mensajes que contengan el #P2PLN al canal determinado en la configuracion del proyecto.\n\nEl repositorio publico del proyecto lo puedes encontrar en https://github.com/franklinzerocr/sevex_bot');
 });
+
 bot.hears('#P2PLN', (ctx) => {
-  console.log(ctx.update.message);
   ctx.forwardMessage(config.channel);
 });
+
 bot.hears('#p2pln', (ctx) => {
-  console.log(ctx.update.message);
   ctx.forwardMessage(config.channel);
 });
 
